02e0246efb90f643c06e39eb2f8bbd4cff56ceea
AayushKing <aayushkingofficial@gmail.com>
2024-10-12 12:22:14 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -1592,6 +1592,7 @@ var cnames_active = {
   "jsonui": "yourtion.github.io/vue-json-ui-editor",
   "jsonuri": "aligay.github.io/jsonuri",
   "jsp": "hiren2001.github.io/jsp",
+  "jstandard": "aayushking.github.io/jstandard",
   "jstyle": "lxhom.github.io/jstyle", // noCF
   "jsxcad": "jsxcad.github.io",
   "juancarlosqr": "juancarlosqr.github.io", // noCF? (don´t add this in a new PR)
